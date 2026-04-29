29ad686fa0f02ac5ba98e0f23923af2ba7998bc0
sae <sae@tega.ru>
2026-04-29 16:34:32 +0300

    Пишем TickerUserService

--- src/application/services/ticket_user_service.py
+++ src/application/services/ticket_user_service.py
@@ -1,5 +1,7 @@
-from src.application.dto.ticket_dto import TicketUserResponseDTO
+from src.application.assemblers.assembler import TicketUserAssembler
+from src.application.dto.ticket_dto import TicketUserResponseDTO, TicketDTO, TicketUserDTO
 from src.application.helper.actor_helper import EmployeeActorHelper
+from src.domain.rbac.permissions import AdminPermission, UserPermission
 from src.domain.ticket_user import TicketUser
 from src.services.uow.uow import UnitOfWork
 
@@ -11,10 +13,39 @@ class TicketUserApplicationService:
 
     def _save_and_to_dto(self, ticket_user: TicketUser) -> TicketUserResponseDTO:
         saved_ticket = self.uow.user_tickets.save(ticket=ticket_user)
-        return TicketAssembler.to_dto(saved_ticket)
+        return TicketUserAssembler.to_dto(saved_ticket)
 
-    def _require_actor_for_create(self, ticket_dto: TicketDTO):
-        actor = self.actor.require_actor_admin(actor_admin_id=ticket_dto.actor_admin_id,
-                                               permission=AdminPermission.CREATE_TICKET)
-        return actor
+    def _require_user_actor_for_create(self, ticket_user_dto: TicketUserDTO):
+        user_actor = self.actor.require_actor_user(actor_user_id=ticket_user_dto.user_id,
+                                               permission=UserPermission.CREATE_TICKET)
+        return user_actor
 
+
+    def create_ticket(
+        self,
+        *,
+        ticket_user_dto: TicketUserDTO,
+    ) -> TicketUserResponseDTO:
+
+        with self.uow:
+            self.actor.require_actor_admin(actor_admin_id=ticket_dto.actor_admin_id,
+                                           permission=AdminPermission.CREATE_TICKET)
+
+            self._validate_references(ticket_dto)
+
+            ticket = Ticket.create(
+                ticket_id=0,
+                client_id=ticket_dto.client_id,
+                admin_id=ticket_dto.admin_id,
+                description=ticket_dto.description,
+                text_of_ticket=ticket_dto.text_of_ticket,
+                user_id=ticket_dto.user_id,
+                contact_user_id=ticket_dto.contact_user_id,
+                is_remote=ticket_dto.is_remote,
+                urgency_level=ticket_dto.urgency_level,
+                user_ticket_id=ticket_dto.user_ticket_id,
+                executor_id=ticket_dto.executor_id,
+                comment=ticket_dto.comment,
+            )
+
+            return self._save_and_to_dto(ticket)
