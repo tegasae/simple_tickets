b32be561070483d429f12883cb648187316bbeb3
sae@tega.ru <sae@tega.ru>
2026-05-22 19:43:02 +0300

    Пишем Oauth

--- src/web/routers/auth.py
+++ src/web/routers/auth.py
@@ -2,8 +2,9 @@ from fastapi import APIRouter, Depends, HTTPException, status
 from fastapi.security import OAuth2PasswordRequestForm
 
 from src.domain.account import Account
-from src.web.dependencies.auth import create_access_token
-from src.web.dependencies.services import get_uow
+from src.web.auth.models import RefreshRequest, LogoutRequest
+from src.web.auth_old.services import AuthManager
+from src.web.dependencies.auth import get_auth_manager_admin, get_auth_manager_user
 
 router = APIRouter(prefix="/auth", tags=["auth"])
 
@@ -36,57 +37,117 @@ def _verify_account_password(entity, plain_password: str) -> None:
         )
 
 
-@router.post("/admin/login", response_model=None)
-def admin_login(
-    form: OAuth2PasswordRequestForm = Depends(),
-    uow = Depends(get_uow),
+#@router.post("/admin/login", response_model=None)
+#def admin_login(
+#    form: OAuth2PasswordRequestForm = Depends(),
+#    uow = Depends(get_uow),
+#):
+#    try:
+#        admin = uow.admins.find_by_login(login=form.username)
+#    except Exception:
+#        raise HTTPException(
+#            status_code=status.HTTP_401_UNAUTHORIZED,
+#            detail="Invalid login or password",
+#        )
+#
+#    _verify_account_password(admin, form.password)
+#
+#    token = create_access_token(
+#        subject_id=admin.employee_id,
+#        subject_type="admin",
+#    )
+#
+#    return {
+#        "access_token": token,
+#        "token_type": "bearer",
+#        "subject_type": "admin",
+#        "subject_id": admin.employee_id,
+#    }
+
+
+
+####
+@router.post("/admin/login")
+async def login(
+        form_data: OAuth2PasswordRequestForm = Depends(),
+        auth_manager: AuthManager = Depends(get_auth_manager_admin)
 ):
-    try:
-        admin = uow.admins.find_by_login(login=form.username)
-    except Exception:
-        raise HTTPException(
-            status_code=status.HTTP_401_UNAUTHORIZED,
-            detail="Invalid login or password",
-        )
-
-    _verify_account_password(admin, form.password)
-
-    token = create_access_token(
-        subject_id=admin.employee_id,
-        subject_type="admin",
-    )
-
-    return {
-        "access_token": token,
-        "token_type": "bearer",
-        "subject_type": "admin",
-        "subject_id": admin.employee_id,
-    }
+    #
+    scopes = form_data.scopes if form_data.scopes else []
+    return auth_manager.login(form_data.username, form_data.password, scopes)
 
 
-@router.post("/user/login", response_model=None)
-def user_login(
-    form: OAuth2PasswordRequestForm = Depends(),
-    uow = Depends(get_uow),
+@router.post("/admin/refresh")
+async def refresh(
+        refresh_request: RefreshRequest,
+        auth_manager: AuthManager = Depends(get_auth_manager_admin)
 ):
-    try:
-        user = uow.users.find_by_login(login=form.username)
-    except Exception:
-        raise HTTPException(
-            status_code=status.HTTP_401_UNAUTHORIZED,
-            detail="Invalid login or password",
-        )
+    return auth_manager.refresh(refresh_request.refresh_token)
 
-    _verify_account_password(user, form.password)
 
-    token = create_access_token(
-        subject_id=user.employee_id,
-        subject_type="user",
-    )
+@router.post("/admin/logout")
+async def logout(
+        logout_request: LogoutRequest,
+        auth_manager: AuthManager = Depends(get_auth_manager_admin)
+):
+    auth_manager.logout(refresh_token_id=logout_request.refresh_token)
+    return {"message": "Logged out successfully"}
 
-    return {
-        "access_token": token,
-        "token_type": "bearer",
-        "subject_type": "user",
-        "subject_id": user.employee_id,
-    }
+
+
+
+@router.post("/user/login")
+async def login_user(
+        form_data: OAuth2PasswordRequestForm = Depends(),
+        auth_manager: AuthManager = Depends(get_auth_manager_user)
+):
+    #
+    scopes = form_data.scopes if form_data.scopes else []
+    return auth_manager.login(form_data.username, form_data.password, scopes)
+
+
+@router.post("/user/refresh")
+async def refresh_user(
+        refresh_request: RefreshRequest,
+        auth_manager: AuthManager = Depends(get_auth_manager_user)
+):
+    return auth_manager.refresh(refresh_request.refresh_token)
+
+
+@router.post("/user/logout")
+async def logout(
+        logout_request: LogoutRequest,
+        auth_manager: AuthManager = Depends(get_auth_manager_user)
+):
+    auth_manager.logout(refresh_token_id=logout_request.refresh_token)
+    return {"message": "Logged out successfully"}
+
+
+
+
+#@router.post("/user/login", response_model=None)
+#def user_login(
+#    form: OAuth2PasswordRequestForm = Depends(),
+#    uow = Depends(get_uow),
+#):
+#    try:
+#        user = uow.users.find_by_login(login=form.username)
+#    except Exception:
+#        raise HTTPException(
+#            status_code=status.HTTP_401_UNAUTHORIZED,
+#            detail="Invalid login or password",
+#        )
+
+#    _verify_account_password(user, form.password)
+#
+#    token = create_access_token(
+#        subject_id=user.employee_id,
+#        subject_type="user",
+#    )
+
+#    return {
+#        "access_token": token,
+#        "token_type": "bearer",
+#        "subject_type": "user",
+#        "subject_id": user.employee_id,
+#    }
